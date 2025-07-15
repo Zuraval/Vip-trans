5dd664be2c8c23837304e96e6fe952d8824db09f
Vladimir Guran <vladimirguran08@gmail.com>
2025-07-15 08:31:15 +0500

    fix + modal callback

--- script.js
+++ script.js
@@ -34,14 +34,12 @@ document.addEventListener('DOMContentLoaded', function () {
   const navLinks = document.querySelectorAll('.navigation-list li');
   const logo = document.querySelector('.header-logo');
 
-  // Функция для установки активного пункта
   function setActiveLink() {
     const currentUrl = window.location.href;
 
     navLinks.forEach(link => {
       const linkHref = new URL(link.closest('a').href, window.location.origin).href;
 
-      // Сравниваем полные URL
       if (currentUrl === linkHref) {
         link.classList.add('active');
       } else {
@@ -65,4 +63,67 @@ document.addEventListener('DOMContentLoaded', function () {
       navLinks.forEach(li => li.classList.remove('active'));
     });
   }
+});
+
+const phoneInput = document.querySelector("input[name='phone']");
+const maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+const phoneMask = IMask(phoneInput, maskOptions);
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+  const inputs = document.querySelectorAll(".scroll-image_text-block__inputs input");
+  const errorDiv = document.getElementById("errorMessages");
+  let errors = [];
+
+  errorDiv.innerHTML = "";
+  inputs.forEach(input => input.classList.remove("error"));
+
+  inputs.forEach(input => {
+    const value = input.value.trim();
+    const name = input.getAttribute("name");
+
+    if (!value || (name === 'phone' && value === '+7()___-__-__')) {
+      errors.push(`Поле "${input.placeholder}" не может быть пустым.`);
+      input.classList.add("error");
+    }
+
+    if (name === 'phone') {
+      const unmaskedValue = phoneMask.unmaskedValue;
+      if (unmaskedValue.length < 10) {
+        errors.push("Введите корректный телефон (минимум 10 цифр).");
+        input.classList.add("error");
+      }
+    }
+  });
+
+  if (errors.length > 0) {
+    errorDiv.innerHTML = "<ul><li>" + errors.join("</li><li>") + "</li></ul>";
+  } else {
+
+  }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Получаем элементы
+  const modalOverlay = document.getElementById('modalOverlay');
+  const openButtons = document.querySelectorAll('.open-modal-btn');
+  const closeBtn = document.getElementById('closeModalBtn');
+
+  openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      modalOverlay.style.display = 'flex';
+    });
+  });
+
+  closeBtn.addEventListener('click', () => {
+    modalOverlay.style.display = 'none';
+  });
+
+  modalOverlay.addEventListener('click', (e) => {
+    if (e.target === modalOverlay) {
+      modalOverlay.style.display = 'none';
+    }
+  });
 });
